4d570538a4e51ebe2e370854397d3c816270ae5c
Mayank <mayank@Mayanks-MacBook-Pro.local>
2026-04-06 09:32:11 +0530

    Day1-1

---
+++ Day_1/Diamond_Problem.java
@@ -0,0 +1,36 @@
+package Day_1;
+
+public class Diamond_Problem {
+	public static void main(String[] args) {
+		Bat bat = new Bat();
+
+		bat.eat();
+		bat.walk();
+		bat.fly();
+		bat.sound();
+	}
+}
+
+class Animal {
+	void eat() {
+		System.out.println("Animal eats");
+	}
+}
+
+class Mammel extends Animal {
+	void walk() {
+		System.out.println("Mammel walks");
+	}
+}
+
+class WingedMammel extends Mammel {
+	void fly() {
+		System.out.println("WingedMammel flies");
+	}
+}
+
+class Bat extends WingedMammel {
+	void sound() {
+		System.out.println("Bat makes ultrasonic sounds");
+	}
+}
